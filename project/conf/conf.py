aa5a77e84eb5bf01c814806a667493d7b740ab88
señor47 <93911255+lord-47@users.noreply.github.com>
2024-05-19 15:13:16 +0200

    feat - Update conf

---
+++ project/conf/conf.py
@@ -0,0 +1,38 @@
+user_dic = {
+    "x": {
+        "username": "x",
+        "password": "x",
+        "email": "x",
+        "account_password": "x"
+    }
+}
+
+llm_dict = {
+    "openAI": {
+        "key": "x",
+        "content": "You are an expert assistant specialized in analyzing tweets. Your task is to provide a "
+                   "comprehensive analysis based on the given prompt and tweet content. Be detailed and precise in "
+                   "your response.",
+        "prompts": {
+            "sentimentAnalysis": "Analiza el sentimiento del siguiente tweet. Proporciona un análisis detallado que "
+                                 "incluya lo siguiente: la emoción principal (positiva, negativa o neutral), "
+                                 "los elementos específicos del texto que contribuyen a ese sentimiento, y una breve "
+                                 "explicación sobre por qué el sentimiento se clasifica de esa manera. Si hay algún "
+                                 "matiz o cambio de tono en el tweet, menciónalo también.\n\nTweet: \"\"\"\n{"
+                                 "text_tweet}\n\"\"\"\n\nAnálisis de sentimiento:\n1. Emoción principal:\n2. "
+                                 "Elementos específicos:\n3. Explicación del sentimiento:\n4. Matices o cambios de "
+                                 "tono:",
+            "politicalAnalysis": "Analiza el siguiente tweet en el contexto de la política española. Proporciona un "
+                                 "análisis político completo que incluya lo siguiente: la ideología subyacente del "
+                                 "tweet, los matices presentes en el mensaje, y el partido político español más "
+                                 "propenso a apoyar o estar de acuerdo con el contenido del tweet. Explica brevemente "
+                                 "por qué se ha clasificado de esa manera.\n\nTweet: \"\"\"\n{"
+                                 "text_tweet}\n\"\"\"\n\nAnálisis político:\n1. Ideología subyacente:\n2. Matices del "
+                                 "mensaje:\n3. Partido político propenso a apoyar:\n4. Explicación:",
+            "supportiveTweet": "Lee el siguiente tweet y redacta un nuevo tweet que exprese apoyo y refuerce el "
+                               "mensaje del tweet original. El nuevo tweet debe ser positivo y estar alineado con el "
+                               "contenido y la intención del original.\n\nTweet original: \"\"\"\n{"
+                               "text_tweet}\n\"\"\"\n\nTweet de apoyo:"
+        }
+    }
+}
